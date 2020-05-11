77c99a0f23a7a9681643b0fbe384fafdec21319e
Fe3o4 <sudi141@163.com>
2020-05-12 01:40:50 +0800

    [master]优化构建

--- build/webpack.config.online.js
+++ build/webpack.config.online.js
@@ -7,19 +7,30 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = () => {
     return merge(baseConfig('online'), {
+        mode: 'production',
         output: {
             libraryTarget: 'umd',
             library: '[name]'
         },
-        mode: 'production',
         plugins: [
+            new BundleAnalyzerPlugin(),
             new CleanWebpackPlugin(),
+
         ],
+        externals: {
+            vue: {
+                root: 'Vue',
+                commonjs: 'vue',
+                commonjs2: 'vue',
+                amd: 'vue',
+            }
+        },
         performance: {
             hints: 'warning'
-        }
+        },
     })
 }
